0a07eea595c28562fe8ef5e62742aee38f2f1c9c
Ketler13 <ketler13@yandex.ru>
2016-12-23 02:24:37 +0200

    previous adding commentAdder

--- src/components/CommentAdder.js
+++ src/components/CommentAdder.js
@@ -1,15 +1,39 @@
 import React, { Component, PropTypes }  from 'react'
 
 export default class CommentAdder extends Component {
+  state = {
+    username: '',
+    commentText: ''
+  }
+
   render() {
     return (
       <form>
         <p>Enter your nickname:</p>
-        <p><input type="text" /></p>
+        <p><input type="text" name="user" onChange={this.userChange} /></p>
         <p>Enter your comment:</p>
-        <p><textarea rows="2" cols="45" name="text"></textarea></p>
-        <p><input type="submit" value="Send comment" onClick = {this.sendCmnt} /></p>
+        <p><textarea rows="2" cols="45" name="text" onChange={this.commentChange}></textarea></p>
+        <p><input type="button" value="Send comment" onClick = {this.addCmnt} /></p>
       </form>
     )
+  }
+
+  userChange = (ev) => {
+    console.log(ev.target.name)
+    this.setState({
+      username: ev.target.value
+    })
+  }
+
+  commentChange = (ev) => {
+    this.setState({
+      commentText: ev.target.value
+    })
+  }
+
+  addCmnt = (ev) => {
+    if (this.state.username && this.state.commentText) {
+      console.log(`User ${this.state.username} send a comment: ${this.state.commentText}`)
+    }
   }
 }
