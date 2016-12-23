adcc45021497588646a99aa2296c4d9e67900a37
Ketler13 <ketler13@yandex.ru>
2016-12-23 13:35:47 +0200

    Modified commentAdder module

--- src/components/CommentAdder.js
+++ src/components/CommentAdder.js
@@ -10,30 +10,30 @@ export default class CommentAdder extends Component {
     return (
       <form>
         <p>Enter your nickname:</p>
-        <p><input type="text" name="user" onChange={this.userChange} /></p>
+        <p><input type="text" name="username" value={this.state.username} onChange={this.inputChange} /></p>
         <p>Enter your comment:</p>
-        <p><textarea rows="2" cols="45" name="text" onChange={this.commentChange}></textarea></p>
+        <p><textarea rows="2" cols="45" name="commentText" value={this.state.commentText} onChange={this.inputChange}></textarea></p>
         <p><input type="button" value="Send comment" onClick = {this.addCmnt} /></p>
       </form>
     )
   }
 
-  userChange = (ev) => {
-    console.log(ev.target.name)
+  inputChange = (ev) => {
+    const inputName = ev.target.name
+    const inputValue = ev.target.value
     this.setState({
-      username: ev.target.value
+      [inputName]: inputValue
     })
   }
 
-  commentChange = (ev) => {
-    this.setState({
-      commentText: ev.target.value
-    })
-  }
 
   addCmnt = (ev) => {
     if (this.state.username && this.state.commentText) {
       console.log(`User ${this.state.username} send a comment: ${this.state.commentText}`)
     }
+    this.setState({
+      username: '',
+      commentText: ''
+    })
   }
 }
