b3bbc47a174be886c787957c662b6e63c82a0091
Roman <kaboom.ua@gmail.com>
2017-01-16 11:13:26 +0200

    Update commentIdGenerator.js

--- src/middlewares/commentIdGenerator.js
+++ src/middlewares/commentIdGenerator.js
@@ -3,6 +3,6 @@ import { addIdToNewComment } from '../helpers'
 
 export default store => next => action => {
     const { type } = action
-
+    //через мидлвары будет проходить каждый экшин, они должны быть максимально общими, завязывать на конкретные экшины - плохая практика
     next(type === ADD_COMMENT ? addIdToNewComment(action) : action)
 }
