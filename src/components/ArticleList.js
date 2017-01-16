23cc850d069706980af323eafddb667735fc779c
Ketler13 <ketler13@yandex.ru>
2017-01-16 23:12:11 +0200

    simple api add

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -4,8 +4,12 @@ import Article from './Article'
 import accordion from '../decorators/accordion'
 import { connect } from 'react-redux'
 import { mapToArray } from '../helpers'
+import { loadAllArticles } from '../AC'
 
 class ArticleList extends React.Component {
+    componentDidMount() {
+        this.props.loadAllArticles()
+    }
     render() {
         const {articles, isOpenItem, toggleOpenItem} = this.props
         const articleElements = articles.map(article =>
@@ -53,5 +57,6 @@ export default connect(
         return {
             articles: filteredArticles
         }
-    }
+    },
+    { loadAllArticles }
 )(accordion(ArticleList))
